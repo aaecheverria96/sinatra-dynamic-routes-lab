a3fcf228863164a990973c611b5b9dc957b387cd
Your Username <you@example.com>
2021-01-31 16:10:01 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,29 @@
 require_relative 'config/environment'
+require 'pry'
 
-class App < Sinatra::Base
-  # Write your code here!
 
-end
+
+  class App < Sinatra::Base
+    # Write your code here!
+    get '/reversename/:name' do 
+     @name = params[:name]
+      "#{@name.reverse}"
+    end 
+     
+    get '/square/:number' do 
+      @number = params[:number] 
+      "#{@number.to_i**2}.to_s"
+    end 
+
+    get '/say/:number/:phrase' do 
+   
+      @number = params[:number] 
+      @phrase = params[:phrase]
+      
+      "#{params[:phrase].times(params[:number]}"
+
+    end 
+  
+  
+  
+  end
